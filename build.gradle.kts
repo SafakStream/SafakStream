ace878e1e64f1823d16e0f1c78cd11ffe9d93e44
SafakStream <safakstream@outlook.com>
2026-08-29 02:53:06 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,14 +7,12 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        // Shitpack repo which contains our tools and dependencies
         maven("https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        // Cloudstream gradle plugin which makes everything work and builds plugins
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+        classpath("com.github.recloudstream.gradle:gradle:master-SNAPSHOT")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
@@ -27,38 +25,52 @@ allprojects {
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
+    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(configuration: BaseExtension.() -> Unit) =
+    extensions.getByName<BaseExtension>("android").configuration()
+
 
 subprojects {
+
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
+
     cloudstream {
-        // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        // you can modify it to use other git hosting services, like gitlab
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/user/repo")
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "https://github.com/SafakStream/SafakStream"
+        )
     }
 
+
     android {
+
         namespace = "recloudstream"
+
+        compileSdkVersion(35)
 
         defaultConfig {
             minSdk = 21
-            compileSdkVersion(35)
             targetSdk = 35
         }
 
+
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_1_8
-            targetCompatibility = JavaVersion.VERSION_1_8
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
         }
 
+
         tasks.withType<KotlinJvmCompile> {
+
             compilerOptions {
-                jvmTarget.set(JvmTarget.JVM_1_8) // Required
+
+                jvmTarget.set(JvmTarget.JVM_17)
+
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -68,23 +80,47 @@ subprojects {
         }
     }
 
+
     dependencies {
+
         val implementation by configurations
 
-        implementation("com.github.recloudstream.cloudstream:library:-SNAPSHOT")
 
-        // These dependencies can include any of those which are added by the app,
-        // but you don't need to include any of them if you don't need them.
-        // https://github.com/recloudstream/cloudstream/blob/master/app/build.gradle.kts
-        implementation(kotlin("stdlib")) // Adds Standard Kotlin Features
-        implementation("com.github.Blatzar:NiceHttp:0.4.11") // HTTP Lib
-        implementation("org.jsoup:jsoup:1.18.3") // HTML Parser
-        // IMPORTANT: Do not bump Jackson above 2.13.1, as newer versions will
-        // break compatibility on older Android devices.
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1") // JSON Parser
-        implementation("com.github.teamnewpipe:NewPipeExtractor:v0.25.2") // NewPipe Extractor
+        implementation(
+            "com.github.recloudstream.cloudstream:library:-SNAPSHOT"
+        )
+
+
+        implementation(kotlin("stdlib"))
+
+
+        implementation(
+            "com.github.Blatzar:NiceHttp:0.4.11"
+        )
+
+
+        implementation(
+            "org.jsoup:jsoup:1.18.3"
+        )
+
+
+        implementation(
+            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
+        )
+
+
+        implementation(
+            "com.github.teamnewpipe:NewPipeExtractor:v0.25.2"
+        )
+
+
+        // Kekik eklentileri için gerekli
+        implementation(
+            "com.github.Kekik:CloudflareKiller:master-SNAPSHOT"
+        )
     }
 }
+
 
 task<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
