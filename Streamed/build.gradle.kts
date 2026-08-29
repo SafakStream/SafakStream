8494e1284f1addde47fcc4b680b9fcd35c8aa611
SafakStream <safakstream@outlook.com>
2026-08-29 18:37:10 +0300

    Update build.gradle.kts

--- Streamed/build.gradle.kts
+++ Streamed/build.gradle.kts
@@ -5,7 +5,22 @@ cloudstream {
     authors     = listOf("kraptor")
     language    = "en"
     description = "Streamed ile Canlı Spor Yayınlarını İzleyebilirsiniz."
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Live") //Movie, AnimeMovie, TvSeries, Cartoon, Anime, OVA, Torrent, Documentary, AsianDrama, Live, NSFW, Others, Music, AudioBook, CustomMedia, Audio, Podcast,
-    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://streamed.pk/&size=128"
+    status      = 1
+    tvTypes     = listOf("Live")
+    iconUrl     = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://streamed.pk/&size=128"
+}
+
+android {
+    namespace = "com.kraptor.streamed"
+
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
