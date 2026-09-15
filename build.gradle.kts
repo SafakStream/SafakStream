db68e175995e7e24498d6aca0c2409d205b7914e
SafakStream <safakstream@outlook.com>
2026-09-15 20:49:32 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,7 @@ import org.gradle.kotlin.dsl.register
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
-
 buildscript {
-
     repositories {
         google()
         mavenCentral()
@@ -15,15 +13,14 @@ buildscript {
     }
 
     dependencies {
-        classpath("com.android.tools.build:gradle:9.1.1")
-        classpath("com.github.recloudstream.gradle:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.0")
+        classpath("com.android.tools.build:gradle:8.7.3")
+        // master-SNAPSHOT yerine JitPack'in doğrudan indirdiği çalışan commit:
+        classpath("com.github.recloudstream:gradle:c4ebbe5ce5")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
     }
 }
 
-
 allprojects {
-
     repositories {
         google()
         mavenCentral()
@@ -31,76 +28,52 @@ allprojects {
     }
 }
 
-
 fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) =
     extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
 
-
 fun Project.android(configuration: LibraryExtension.() -> Unit) {
-
     extensions.getByName<LibraryExtension>("android").apply {
-
         project.extensions.findByType(JavaPluginExtension::class.java)?.apply {
-
             toolchain {
                 languageVersion.set(JavaLanguageVersion.of(17))
             }
         }
-
         configuration()
     }
 }
 
-
 subprojects {
-
     apply(plugin = "com.android.library")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
-
     cloudstream {
-
         setRepo(
             System.getenv("GITHUB_REPOSITORY")
                 ?: "https://github.com/SafakStream/SafakStream"
         )
-
         authors = listOf("SafakStream")
     }
 
-
     android {
-
         namespace = "com.keyiflerolsun"
-
-        compileSdk = 36
-
+        compileSdk = 35
 
         defaultConfig {
-
             minSdk = 21
         }
 
-
         lint {
-
-            targetSdk = 36
+            targetSdk = 35
         }
 
-
         compileOptions {
-
             sourceCompatibility = JavaVersion.VERSION_17
             targetCompatibility = JavaVersion.VERSION_17
         }
 
-
         tasks.withType<KotlinJvmCompile> {
-
             compilerOptions {
-
                 jvmTarget.set(JvmTarget.JVM_17)
-
                 freeCompilerArgs.addAll(
                     "-Xno-call-assertions",
                     "-Xno-param-assertions",
@@ -110,90 +83,29 @@ subprojects {
         }
     }
 
-
     dependencies {
-
         val implementation by configurations
         val cloudstream by configurations
 
-
-        cloudstream(
-            "com.lagradost:cloudstream3:pre-release"
-        )
-
+        cloudstream("com.lagradost:cloudstream3:pre-release")
 
         implementation(kotlin("stdlib"))
-
-
-        implementation(
-            "com.github.Blatzar:NiceHttp:0.4.18"
-        )
-
-
-        implementation(
-            "org.jsoup:jsoup:1.22.2"
-        )
-
-
-        // NewPipeExtractor - YouTube Provider
-        implementation(
-            "com.github.teamnewpipe:NewPipeExtractor:v0.25.2"
-        )
-
-
-        implementation(
-            "org.jspecify:jspecify:1.0.0"
-        )
-
-
-        implementation(
-            "androidx.annotation:annotation:1.10.0"
-        )
-
-
-        implementation(
-            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
-        )
-
-
-        implementation(
-            "com.fasterxml.jackson.core:jackson-databind:2.13.1"
-        )
-
-
-        implementation(
-            "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2"
-        )
-
-
-        implementation(
-            "org.mozilla:rhino:1.8.1"
-        )
-
-
-        implementation(
-            "me.xdrop:fuzzywuzzy:1.4.0"
-        )
-
-
-        implementation(
-            "com.google.code.gson:gson:2.14.0"
-        )
-
-
-        implementation(
-            "org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0"
-        )
-
-
-        implementation(
-            "org.bouncycastle:bcpkix-jdk18on:1.84"
-        )
+        implementation("com.github.Blatzar:NiceHttp:0.4.18")
+        implementation("org.jsoup:jsoup:1.22.2")
+        implementation("com.github.teamnewpipe:NewPipeExtractor:v0.25.2")
+        implementation("org.jspecify:jspecify:1.0.0")
+        implementation("androidx.annotation:annotation:1.9.1")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+        implementation("org.mozilla:rhino:1.8.1")
+        implementation("me.xdrop:fuzzywuzzy:1.4.0")
+        implementation("com.google.code.gson:gson:2.14.0")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.8.0")
+        implementation("org.bouncycastle:bcpkix-jdk18on:1.84")
     }
 }
 
-
 tasks.register<Delete>("clean") {
-
     delete(rootProject.layout.buildDirectory)
 }
