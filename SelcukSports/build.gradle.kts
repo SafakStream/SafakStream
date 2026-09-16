9684a0ed6adf5dda834b6b207408c64b615bc7c0
SafakStream <safakstream@outlook.com>
2026-09-16 15:28:16 +0300

    Create build.gradle.kts

---
+++ SelcukSports/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 12
+
+cloudstream {
+    authors     = listOf("SafakStream")
+    language    = "tr"
+    description = "Canlı spor yayınları ve maç özetleri izle."
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1
+    tvTypes = listOf("Live")
+    iconUrl = "https://www.google.com/s2/favicons?domain=selcuksportshd.com&sz=%size%"
+}
